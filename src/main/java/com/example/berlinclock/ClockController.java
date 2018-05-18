78612a0167ab3c5f0b29c6c8b6a30a81a181cd67
brookehau <brookerhauser@gmail.com>
2018-05-18 15:42:34 -0400

    added clock to html

--- src/main/java/com/example/berlinclock/ClockController.java
+++ src/main/java/com/example/berlinclock/ClockController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ClockController {
 
-	@RequestMapping("/clock")
+	@RequestMapping("/index")
 	public String showClock() {
 		return "index";
 	}
