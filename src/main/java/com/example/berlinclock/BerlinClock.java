6379563de09940261e5e7e42a7ddca4e7a4e6b0c
brookehau <brookerhauser@gmail.com>
2018-05-18 07:59:11 -0400

    tested more hours, minutes and seconds

--- src/main/java/com/example/berlinclock/BerlinClock.java
+++ src/main/java/com/example/berlinclock/BerlinClock.java
@@ -12,10 +12,6 @@ public class BerlinClock {
 		this.seconds = seconds;
 	}
 
-	/*public int getSeconds() {
-		return seconds;
-	}*/
-
 	public String toplampColor() {
 		if (seconds % 2 == 0) {
 			return "Y";
@@ -28,11 +24,87 @@ public class BerlinClock {
 	}
 
 	public String topRowLightColor(int lightNumber) {
-		if(hour>=5 && lightNumber==1 ) {
+		if (hour >= 20 && lightNumber == 4) {
 			return "R";
 		}
-		if(hour>=10 && lightNumber==2 ) {
+		if (hour >= 15 && lightNumber == 3) {
 			return "R";
+		}
+		if (hour >= 10 && lightNumber == 2) {
+			return "R";
+		}
+		if (hour >= 5 && lightNumber == 1) {
+			return "R";
+		}
+
+		return "O";
+	}
+
+	public String secondRowLightColor(int lightNumber) {
+		if (hour >= 24 || (hour >= 4 && hour < 5) && lightNumber == 4) {
+			return "R";
+		}
+		if (hour >= 23 || (hour >= 3 && hour < 5) && lightNumber == 3) {
+			return "R";
+		}
+		if (hour >= 22 || (hour >= 2 && hour < 5) && lightNumber == 2) {
+			return "R";
+		}
+		if (hour >= 21 || (hour >= 1 && hour < 5) && lightNumber == 1) {
+			return "R";
+		}
+		return "O";
+	}
+
+	public String firstRowMinutesLightColor(int lightNumber) {
+		if (minutes >= 55 && lightNumber == 11) {
+			return "Y";
+		}
+		if (minutes >= 50 && lightNumber == 10) {
+			return "Y";
+		}
+		if (minutes >= 45 && lightNumber == 9) {
+			return "Y";
+		}
+		if (minutes >= 40 && lightNumber == 8) {
+			return "Y";
+		}
+		if (minutes >= 35 && lightNumber == 7) {
+			return "Y";
+		}
+		if (minutes >= 30 && lightNumber == 6) {
+			return "Y";
+		}
+		if (minutes >= 25 && lightNumber == 5) {
+			return "Y";
+		}
+		if (minutes >= 20 && lightNumber == 4) {
+			return "Y";
+		}
+		if (minutes >= 15 && lightNumber == 3) {
+			return "Y";
+		}
+		if (minutes >= 10 && lightNumber == 2) {
+			return "Y";
+		}
+		if (minutes >= 5 && lightNumber == 1) {
+			return "Y";
+		}
+		return "O";
+	}
+
+	public String secondRowMinutesLightColor(int lightNumber) {
+		if (minutes >= 59 && lightNumber == 4) {
+			return "Y";
+		}
+		if (minutes >= 58 && lightNumber == 3) {
+			return "Y";
+		}
+		if (minutes >= 57 && lightNumber == 2) {
+			return "Y";
+		}
+		if (minutes >= 56 && lightNumber == 1) {
+			return "Y";
 		}
 		return "O";
 	}
--- src/test/java/com/example/berlinclock/BerlinTest.java
+++ src/test/java/com/example/berlinclock/BerlinTest.java
@@ -45,13 +45,135 @@ public class BerlinTest {
 	public void firstTopRowSecondtLightShouldBeOff() {
 		BerlinClock clock = new BerlinClock(1, 25, 1);
 		String lights = clock.topRowLightColor(2);
+		String light = clock.topRowLightColor(1);
 		assertThat(lights, is("O"));
+		assertThat(light, is("O"));
 	}
 	
 	@Test
 	public void firstTopRowSecondtLightShouldBeRed() {
 		BerlinClock clock = new BerlinClock(11, 25, 1);
 		String lights = clock.topRowLightColor(2);
+		String light = clock.topRowLightColor(1);
 		assertThat(lights, is("R"));
+		assertThat(light, is("R"));
+	}
+	
+	@Test
+	public void firstTopRowThirdLightShouldBeOff() {
+		BerlinClock clock = new BerlinClock(11, 25, 1);
+		String lights = clock.topRowLightColor(3);
+		String light = clock.topRowLightColor(1);
+		assertThat(lights, is("O"));
+		assertThat(light, is("R"));
+	}
+	
+	@Test
+	public void firstTopRowThirdLightShouldBeRed() {
+		BerlinClock clock = new BerlinClock(15, 25, 1);
+		String lights = clock.topRowLightColor(3);
+		String light = clock.topRowLightColor(2);
+		assertThat(lights, is("R"));
+		assertThat(light, is("R"));
+	}
+	
+	@Test
+	public void firstTopRowFourthLightShouldBeOff() {
+		BerlinClock clock = new BerlinClock(15, 25, 1);
+		String lights = clock.topRowLightColor(4);
+		String light = clock.topRowLightColor(2);
+		assertThat(lights, is("O"));
+		assertThat(light, is("R"));
+	}
+	
+	@Test
+	public void firstTopRowFourthLightShouldBeRed() {
+		BerlinClock clock = new BerlinClock(20, 25, 1);
+		String lights = clock.topRowLightColor(4);
+		String light = clock.topRowLightColor(2);
+		String light2 = clock.secondRowLightColor(1);
+		assertThat(lights, is("R"));
+		assertThat(light, is("R"));
+		assertThat(light2, is("O"));
+	}
+	@Test
+	public void testingMoreFromAbove() {
+		BerlinClock clock = new BerlinClock(4, 25, 1);
+		String lights = clock.topRowLightColor(4);
+		String light = clock.topRowLightColor(2);
+		String light2 = clock.secondRowLightColor(1);
+		assertThat(lights, is("O"));
+		assertThat(light, is("O"));
+		assertThat(light2, is("R"));
+	}
+	
+	@Test
+	public void secondTopRowFirstLightShouldBeRed() {
+		BerlinClock clock = new BerlinClock(21, 25, 1);
+		String lights = clock.topRowLightColor(4);
+		String light = clock.secondRowLightColor(1);
+		assertThat(lights, is("R"));
+		assertThat(light, is("R"));
+	}
+	
+	@Test
+	public void firstRowFirstLightOfMinutesShouldBeOff() {
+		BerlinClock clock = new BerlinClock(21, 1, 1);
+		String lights = clock.topRowLightColor(4);
+		String light = clock.firstRowMinutesLightColor(1);
+		String light2 = clock.firstRowMinutesLightColor(2);
+		assertThat(lights, is("R"));
+		assertThat(light, is("O"));
+		assertThat(light2, is("O"));
+	}
+	@Test
+	public void firstRowFirstLightOfMinutesShouldBeY() {
+		BerlinClock clock = new BerlinClock(21, 15, 1);
+		String lights = clock.topRowLightColor(4);
+		String light = clock.firstRowMinutesLightColor(1);
+		String light2 = clock.firstRowMinutesLightColor(2);
+		String light3 = clock.firstRowMinutesLightColor(4);
+		String light4 = clock.secondRowMinutesLightColor(2);
+		assertThat(lights, is("R"));
+		assertThat(light, is("Y"));
+		assertThat(light2, is("Y"));
+		assertThat(light3, is("O"));
+		assertThat(light4, is("O"));
+	}
+	@Test
+	public void secondRowFirstLightOfMinutesShouldBeY() {
+		BerlinClock clock = new BerlinClock(21, 56, 1);
+		String lights = clock.topRowLightColor(4);
+		String light = clock.firstRowMinutesLightColor(1);
+		String light2 = clock.firstRowMinutesLightColor(2);
+		String light3 = clock.firstRowMinutesLightColor(4);
+		String light4 = clock.secondRowMinutesLightColor(1);
+		String light5 = clock.secondRowMinutesLightColor(2);
+		assertThat(lights, is("R"));
+		assertThat(light, is("Y"));
+		assertThat(light2, is("Y"));
+		assertThat(light3, is("Y"));
+		assertThat(light4, is("Y"));
+		assertThat(light5, is("O"));
+	}
+	@Test
+	public void testingEverything() {
+		BerlinClock clock = new BerlinClock(1, 56, 1);
+		String seconds = clock.toplampColor();
+		String lights = clock.topRowLightColor(4);
+		String test = clock.secondRowLightColor(1);
+		String light = clock.firstRowMinutesLightColor(1);
+		String light2 = clock.firstRowMinutesLightColor(2);
+		String light3 = clock.firstRowMinutesLightColor(4);
+		String light4 = clock.secondRowMinutesLightColor(1);
+		String light5 = clock.secondRowMinutesLightColor(2);
+		assertThat(seconds, is("O"));
+		assertThat(lights, is("O"));
+		assertThat(test, is("R"));
+		assertThat(light, is("Y"));
+		assertThat(light2, is("Y"));
+		assertThat(light3, is("Y"));
+		assertThat(light4, is("Y"));
+		assertThat(light5, is("O"));
 	}
 }
